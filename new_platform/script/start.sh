db2064bfc2d0463ca6e59c5978ac5d3403aedbb6
Roberta Takenaka <roberta.takenaka@scielo.org>
2024-02-07 18:45:32 -0300

    Adiciona instrução para gerar arquivos .id para bases de serial

--- new_platform/script/start.sh
+++ new_platform/script/start.sh
@@ -1,4 +1,4 @@
-chmod +x new_platform/script/*.sh
+chmod +x new_platform/script/*/*.sh
 
 if [ ! -f proc/cisis/i2id ]; then
     echo "migration must be installed in the same directory as bases, bases-work and proc"
@@ -21,6 +21,13 @@ proc/cisis/i2id bases/title/title > new_platform/bases_for_upload/title.id
 echo "Create new_platform/bases_for_upload/issue.id"
 proc/cisis/i2id bases/issue/issue > new_platform/bases_for_upload/issue.id
 
+if [ -d serial ]; then
+    echo "Create id files for serial"
+    echo "...."
+    ./new_platform/script/serial/serial.sh
+    echo "...."
+fi
+
 echo "Create script ./new_platform/tmp/create_bases_work_acron.sh"
 proc/cisis/mx bases/title/title lw=9999 "pft=@new_platform/script/bases_work_acron/bases_work_acron.pft" now | tr '[:upper:]' '[:lower:]' | sort -u > new_platform/tmp/create_bases_work_acron.sh
 
@@ -39,7 +46,7 @@ if [ -d bases/artigo/p ]; then
 fi
 
 echo "Created id files"
-....
+echo "...."
 find new_platform/bases_for_upload -name "*.id"
 
 
